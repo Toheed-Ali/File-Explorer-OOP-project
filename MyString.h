5f5368ba263cdfce0d36133f3c05d48828cad72f
Toheed Ali <bscs24119@itu.edu.pk>
2025-06-13 01:28:09 +0500

    String Library

--- MyString.h
+++ MyString.h
@@ -1,9 +1,107 @@
 #ifndef STRING_H
 #define STRING_H
 #include <iostream>
-#include <cstring>
-#include <vector>
+#include <string>
+#include "MyVector.h"
 using namespace std;
+
+int mystrlen(const char* str)
+{
+    int length = 0;
+    while (str[length] != '\0')
+    {
+        length++;
+    }
+    return length;
+}
+
+char* mystrstr(const char* haystack, const char* needle)
+{
+	if (*needle == '\0')
+	{
+		return (char*)haystack;
+	}
+
+	int haystackLen = mystrlen(haystack);
+	int needleLen = mystrlen(needle);
+
+	for (int i = 0; i <= haystackLen - needleLen; i++)
+	{
+		int j = 0;
+		while (j < needleLen && haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+
+		if (j == needleLen)
+		{
+			return (char*)(haystack + i);
+		}
+	}
+
+	return nullptr;
+}
+
+
+char* mystrcpy(char* dest, const char* src)
+{
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
+char* mystrncpy(char* dest, const char* src, int n)
+{
+    int i = 0;
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return dest;
+}
+
+int mystrcmp(const char* str1, const char* str2)
+{
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0')
+    {
+        if (str1[i] != str2[i])
+        {
+            return str1[i] - str2[i];
+        }
+        i++;
+    }
+    return str1[i] - str2[i];
+}
+
+char* mystrcat(char* dest, const char* src)
+{
+    int i = 0;
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    int j = 0;
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
 
 class MyString
 {
@@ -21,23 +119,23 @@ public:
 
 	MyString(const char* str)
 	{
-		size = strlen(str);
+		size = mystrlen(str);
 		data = new char[size + 1];
-		strcpy(data, str);
+		mystrcpy(data, str);
 	}
 
 	MyString(const string& str)
 	{
 		size = str.length();
 		data = new char[size + 1];
-		strcpy(data, str.c_str());
+		mystrcpy(data, str.c_str());
 	}
 
 	MyString(const MyString& other)
 	{
 		size = other.size;
 		data = new char[size + 1];
-		strcpy(data, other.data);
+		mystrcpy(data, other.data);
 	}
 
 	~MyString()
@@ -52,7 +150,7 @@ public:
 			delete[] data;
 			size = other.size;
 			data = new char[size + 1];
-			strcpy(data, other.data);
+			mystrcpy(data, other.data);
 		}
 		return *this;
 	}
@@ -63,14 +161,14 @@ public:
 		result.size = size + other.size;
 		delete[] result.data;
 		result.data = new char[result.size + 1];
-		strcpy(result.data, data);
-		strcat(result.data, other.data);
+		mystrcpy(result.data, data);
+		mystrcat(result.data, other.data);
 		return result;
 	}
 
 	bool operator==(const MyString& other) const
 	{
-		return strcmp(data, other.data) == 0;
+		return mystrcmp(data, other.data) == 0;
 	}
 
 	bool operator!=(const MyString& other) const
@@ -101,7 +199,7 @@ public:
 		}
 
 		char* temp = new char[len + 1];
-		strncpy(temp, data + pos, len);
+		mystrncpy(temp, data + pos, len);
 		temp[len] = '\0';
 
 		MyString result(temp);
@@ -109,14 +207,9 @@ public:
 		return result;
 	}
 
-	MyString substring(int pos, int len) const
-	{
-		return substr(pos, len);
-	}
-
 	int find(const MyString& str) const
 	{
-		char* pos = strstr(data, str.data);
+		char* pos = mystrstr(data, str.data);
 		if (pos)
 			return pos - data;
 		return -1;
@@ -137,9 +230,9 @@ public:
 		if (pos > size) pos = size;
 
 		char* newData = new char[size + str.size + 1];
-		strncpy(newData, data, pos);
-		strcpy(newData + pos, str.data);
-		strcpy(newData + pos + str.size, data + pos);
+		mystrncpy(newData, data, pos);
+		mystrcpy(newData + pos, str.data);
+		mystrcpy(newData + pos + str.size, data + pos);
 
 		delete[] data;
 		data = newData;
@@ -148,7 +241,7 @@ public:
 
 	int compare(const MyString& other) const
 	{
-		return strcmp(data, other.data);
+		return mystrcmp(data, other.data);
 	}
 
 	void replace(int pos, int len, const MyString& str)
@@ -157,9 +250,9 @@ public:
 		if (pos + len > size) len = size - pos;
 
 		char* newData = new char[size - len + str.size + 1];
-		strncpy(newData, data, pos);
-		strcpy(newData + pos, str.data);
-		strcpy(newData + pos + str.size, data + pos + len);
+		mystrncpy(newData, data, pos);
+		mystrcpy(newData + pos, str.data);
+		mystrcpy(newData + pos + str.size, data + pos + len);
 		delete[] data;
 		data = newData;
 		size = size - len + str.size;
